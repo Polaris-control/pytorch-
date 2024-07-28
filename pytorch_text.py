f4456e88bd92070b1ac1c6985db2fbeafc46f330
HU kaifeng <hukaifeng@pusan.ac.kr>
2024-07-28 23:17:45 +0900

    Fix line endings

--- pytorch_text.py
+++ pytorch_text.py
@@ -1,8 +1,64 @@
 import torch
-torch.__version__
+import numpy as np
+import torch.nn as nn 
 
-x = torch.empty(5,3)
-print(x)
 
-x = torch.zeros(5,3,dtype=torch.double)
-print(x)
+x_values = [i for i in range(11)]
+x_train = np.array(x_values,dtype = np.float32)
+x_train = x_train.reshape(-1,1)
+x_train.shape
+
+
+
+y_values = [2*i + 1 for i in x_values]
+y_train = np.array(y_values,dtype = np.float32)
+y_train = y_train.reshape(-1,1)
+y_train.shape
+
+class LinearRegressionModel(nn.Module):
+    def __init__(self,input_dim,output_dim):
+        super(LinearRegressionModel,self).__init__()
+        self.linear = nn.Linear(input_dim,output_dim)
+
+        
+
+    def forward(self,x):
+        out = self.linear(x)
+        return out
+
+input_dim = 1 
+output_dim = 1
+model = LinearRegressionModel(input_dim,output_dim)
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model.to(device)
+    
+    
+epochs = 1000
+learning_rate = 0.01
+optimizer = torch.optim.SGD(model.parameters(),lr=learning_rate)
+criterion = nn.MSELoss()
+
+for epoch in range(epochs):
+    epoch += 1
+    inputs = torch.from_numpy(x_train).to(device)
+    labels = torch.from_numpy(y_train).to(device)
+
+    optimizer.zero_grad()
+
+    outputs = model(inputs)
+
+    loss = criterion(outputs,labels)
+
+    loss.backward()
+
+    optimizer.step()
+    if epoch % 50  == 0:
+        print("epoch {}, loss {}".format(epoch,loss.item()))
+
+
+predicted = model(torch.from_numpy(x_train).requires_grad_()).data.numpy()
+print(predicted)
+
+torch.save(model.state_dict(),"model.pkl")
+model.load_state_dict(torch.load("model.pkl"))
